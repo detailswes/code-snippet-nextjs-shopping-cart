0bb6d9b63c02ea5565685a3e2e425e9de9006c00
details.wes <details.wes@gmail.com>
2023-11-28 18:06:20 +0530

    cart counter added

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -71,7 +71,8 @@ export default function Header() {
           </Link>           
           
         </Popover.Group>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end relative">
+          <div className='absolute bg-indigo-600 hover:bg-indigo-700 w-5 h-5 flex items-center justify-center rounded-full right-[-10px] top-[-10px]'><p className='text-xs text-white'>2</p></div>
           <div onClick={toggleSideBar} className="cursor-pointer text-sm font-semibold leading-6 text-gray-900">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
