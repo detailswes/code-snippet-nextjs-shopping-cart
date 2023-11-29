437e2899186bd5f952aa2841cc5635a49699e625
detailswes <details.wes@gmail.com>
2023-11-29 11:55:50 +0530

    add design changes

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -73,7 +73,7 @@ export default function Header() {
     <>
       <header className="bg-white">
         <nav
-          className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+          className="mx-auto flex max-w-7xl items-center justify-between py-6 lg:px-0"
           aria-label="Global"
         >
           <div className="flex lg:flex-1">
